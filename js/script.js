2a950ce2df717953bf975819e49f3d9caf5212f4
jsidler <sidlerj@bzz.ch>
2021-11-05 14:38:00 +0100

    Formularüberprüfung bei Kontaktformular fertig + E-Mail-Kontrolle mit Punkt und @

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ const email = document.getElementById('Email');
 
 let formFields = [fullName, email]
 
-
 function errorMsg() {
     if (document.getElementById('fullname').value == "") {
         document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
-    } if (document.getElementById('email').value == "") {
-        document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
-    } if (document.getElementById('betreff').value == "") {
+    } //if (document.getElementById('email').value == "") {
+        //document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";}
+if (document.getElementById('betreff').value == "") {
         document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
     } if (document.getElementById('textblock').value == "") {
         document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
@@ -19,10 +18,22 @@ function errorMsg() {
 }
 
 function errorMsgEmail() {
-    if (document.getElementById('email').value.indexOf('@') == -1) {
-        document.getElementById("errorTextMsg").innerHTML = "Die E-Mail-Adresse muss ein @-Zeichen enthalten.";
-    } else {
-        document.getElementById("errorTextMsg").innerHTML = "";
-        return window.document.forms[0].reset();
+    if (document.getElementById('email').value == ""){
+        document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
+    }
+    else {
+        if (document.getElementById('email').value.length < 3){
+            document.getElementById("errorTextMsg").innerHTML = "Die E-Mail-Adresse ist zu kurz.";
+        }
+        else {
+            if (document.getElementById('email').value.indexOf("@") == -1){
+                document.getElementById("errorTextMsg").innerHTML = "Die E-Mail-Adresse muss ein @-Zeichen enthalten.";
+            }
+            else {
+                if (document.getElementById('email').value.indexOf(".", document.getElementById('email').value.indexOf("@")) == -1){
+                    document.getElementById("errorTextMsg").innerHTML = "Nach dem @-Zeichen muss ein Punkt folgen.";
+                }
+            }
+        }
     }
 }
