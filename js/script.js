a33eb40dd310a4ecf322ada19428def17abceb80
jsidler <sidlerj@bzz.ch>
2021-11-05 14:02:58 +0100

    Formularüberprüfung bei Kontaktformular fertig

--- js/script.js
+++ js/script.js
@@ -15,6 +15,14 @@ function errorMsg() {
         document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
     } else {
         document.getElementById("errorTextMsg").innerHTML = "";
+    }
+}
+
+function errorMsgEmail() {
+    if (document.getElementById('email').value.indexOf('@') == -1) {
+        document.getElementById("errorTextMsg").innerHTML = "Die E-Mail-Adresse muss ein @-Zeichen enthalten.";
+    } else {
+        document.getElementById("errorTextMsg").innerHTML = "";
         return window.document.forms[0].reset();
     }
 }
