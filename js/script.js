14eb1934316d3ab3a17f1c2a95a1ad85c2287836
jsidler <sidlerj@bzz.ch>
2021-11-16 15:48:51 +0100

    Formular funktioniert. Email noch nicht. (ohne Classes)

--- js/script.js
+++ js/script.js
@@ -1,38 +1,33 @@
-//Formular Validierung
-const fullName = document.getElementById('fullname');
-const email = document.getElementById('Email');
+// read form element
+const form = document.getElementById('form');
+const fullname = document.getElementById('fullname');
+const email = document.getElementById('email');
+const betreff = document.getElementById('betreff');
+const fileDownload = document.getElementById('fileDownload');
+const textblock = document.getElementById('textblock');
+const errorText = document.getElementById('errorTextMsg');
+const absendenButton = document.getElementById('absenden');
 
-let formFields = [fullName, email]
 
 
-function errorMsg() {
-    if (document.getElementById('fullname').value == "") {
-        document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
-    } if (document.getElementById('betreff').value == "") {
-        document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
-    } if (document.getElementById('textblock').value == "") {
-        document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
-    } else {
-        document.getElementById("errorTextMsg").innerHTML = "";
+function showErrorMsg() {
+    if(fullname.value === '') {
+        errorText.innerHTML = 'Alle mit * markierte Felder müssen ausgefüllt werden!';
+    }
+    else if(email.value === '') {
+        errorText.innerHTML = 'Alle mit * markierte Felder müssen ausgefüllt werden!';
+    }
+    else if(betreff.value === '') {
+        errorText.innerHTML = 'Alle mit * markierte Felder müssen ausgefüllt werden!';
+    }
+    else if(textblock.value === '') {
+        errorText.innerHTML = 'Alle mit * markierte Felder müssen ausgefüllt werden!';
+    }
+    else{
+        errorText.innerHTML = 'Das Formular wurde erfolgreich abgeschickt.';
     }
 }
 
-function errorMsgEmail() {
-    if (document.getElementById('email').value == ""){
-        document.getElementById("errorTextMsg").innerHTML = "Alle mit * markierte Felder müssen ausgefüllt werden!";
-    } else {
-        if (document.getElementById('email').value.length < 5){
-            document.getElementById("errorTextMsg").innerHTML = "Die E-Mail-Adresse ist zu kurz.";
-        }
-        else {
-            if (document.getElementById('email').value.indexOf("@") == -1){
-                document.getElementById("errorTextMsg").innerHTML = "Die E-Mail-Adresse muss ein @-Zeichen enthalten.";
-            }
-            else {
-                if (document.getElementById('email').value.indexOf(".", document.getElementById('email').value.indexOf("@")) == -1){
-                    document.getElementById("errorTextMsg").innerHTML = "Nach dem @-Zeichen muss ein Punkt folgen.";
-                }
-            }
-        }
-    }
-}
+// else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)){
+//         errorText.innerHTML = 'Geben Sie bitte eine richtige E-Mail-Adresse an';
+//     }
